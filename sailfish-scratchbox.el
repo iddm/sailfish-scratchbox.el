df27d242b318a579c041c4dd359f69345098571d
Victor Polevoy <v.polevoy@omprussia.ru>
2017-11-28 15:05:09 +0300

    Add copy rpms command

--- sailfish-scratchbox.el
+++ sailfish-scratchbox.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2017 Victor Polevoy
 
 ;; Author: V. V. Polevoy <fx@thefx.co>
-;; Version: 1.0.2
+;; Version: 1.1.0
 ;; Keywords: sb2, mb2, building, scratchbox, sailfish
 ;; URL: https://github.com/vityafx/sailfish-scratchbox.el
 ;; License: MIT
@@ -51,6 +51,19 @@
   :group 'sailfish-scratchbox
   :safe #'stringp)
 
+(defcustom sailfish-scratchbox-deploy-buffer-name "*scratchbox deploy*"
+  "The sailfish scratchbox build buffer name."
+  :type 'string
+  :group 'sailfish-scratchbox
+  :safe #'stringp)
+
+(defcustom sailfish-scratchbox-deploy-rpms-command "scp RPMS/*.rpm nemo@192.168.2.15:/home/nemo"
+  "The sailfish scratchbox deploy command.
+User must have his identity installed onto the phone is the command invokes scp."
+  :type 'string
+  :group 'sailfish-scratchbox
+  :safe #'stringp)
+
 
 (defun scratchbox-project-root ()
   "Return project root."
@@ -81,11 +94,29 @@ Something like 'sdk mb2 build'"
           (compilation-start command-to-run 'sailfish-scratchbox-compilation-mode (lambda (m) (buffer-name))))))
         (message "(%s) does not seem to be inside a valid sailfish os project." (buffer-name)))
 
+(defun scratchbox-deploy-rpms-run ()
+  "Run the deploy command."
+  (when (get-buffer sailfish-scratchbox-deploy-buffer-name)
+    (kill-buffer sailfish-scratchbox-deploy-buffer-name))
+  (let ((command-to-run sailfish-scratchbox-deploy-rpms-command)
+        (root-dir (scratchbox-project-root)))
+    (if root-dir
+        (with-current-buffer (get-buffer-create sailfish-scratchbox-deploy-buffer-name)
+          (setq default-directory root-dir)
+          (compilation-start command-to-run 'sailfish-scratchbox-compilation-mode (lambda (m) (buffer-name))))))
+        (message "(%s) does not seem to be inside a valid sailfish os project." (buffer-name)))
+
 ;;;###autoload
 (defun sailfish-scratchbox-mb2-build ()
   "Build the project inside the sdk this file is in."
   (interactive)
   (scratchbox-mb2-build-run))
+
+;;;###autoload
+(defun sailfish-scratchbox-deploy-rpms()
+  "Copy the the built project artifacts to the phone."
+  (interactive)
+  (scratchbox-deploy-rpms-run))
 
 (provide 'sailfish-scratchbox)
 ;;; sailfish-scratchbox.el ends here
